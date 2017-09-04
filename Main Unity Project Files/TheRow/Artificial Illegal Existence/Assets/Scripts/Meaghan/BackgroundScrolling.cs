a8318a946fca51d56d10113b244f9acac8ad3bcd
ResidentUnleashed <31601762+ResidentUnleashed@users.noreply.github.com>
2017-09-04 11:07:57 +1000

    Changed camera angle Created scrolling background

---
+++ Main Unity Project Files/TheRow/Artificial Illegal Existence/Assets/Scripts/Meaghan/BackgroundScrolling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour {
+
+    //Varibles
+    public float scrollSpeed = 0.5F;
+    public Renderer rend;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Set up rend
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Creates an offset variable
+        float offset = Time.time * scrollSpeed;
+
+        //Allows for the texture to scroll based on the offset
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
